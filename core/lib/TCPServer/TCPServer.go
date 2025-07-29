dd4a4252291b5371a3924634df765dcb5d66c7f5
Keivan-sf <keyvan0082@gmail.com>
2025-07-29 20:42:49 +0330

    Add broadcast capability to tcp server

---
+++ core/lib/TCPServer/TCPServer.go
@@ -0,0 +1,141 @@
+package TCPServer
+
+import (
+	"bufio"
+	cmd "bushuray-core/commands"
+	"bushuray-core/db"
+	"bushuray-core/structs"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+)
+
+type Server struct {
+	clients map[string]net.Conn
+	DB      *db.DB
+	mutex   sync.RWMutex
+}
+
+func NewServer(database *db.DB) *Server {
+	return &Server{
+		DB:      database,
+		clients: make(map[string]net.Conn),
+	}
+}
+
+func (s *Server) Start() {
+	listen, err := net.Listen("tcp", "127.0.0.1:4897")
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(0)
+	}
+
+	fmt.Println("server is listening on port 4897")
+
+	go func() {
+		for {
+			conn, err := listen.Accept()
+
+			if err != nil {
+				log.Printf("failed to accept connection: %v", err)
+			}
+
+			s.mutex.Lock()
+			clientID := conn.RemoteAddr().String()
+			s.clients[clientID] = conn
+			s.mutex.Unlock()
+
+			go s.handleConnection(conn, clientID)
+		}
+	}()
+}
+
+func (s *Server) BroadCast(msg []byte) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	for clientID, conn := range s.clients {
+		_, err := conn.Write(msg)
+		if err != nil {
+			fmt.Printf("Error sending to %s: %v\n", clientID, err)
+		}
+	}
+}
+
+func (s *Server) handleConnection(conn net.Conn, clientID string) {
+	defer func() {
+		conn.Close()
+		s.mutex.Lock()
+		delete(s.clients, clientID)
+		s.mutex.Unlock()
+		fmt.Println("Disconnected:", clientID)
+	}()
+
+	defer func() {
+		fmt.Println("actually disconnected")
+	}()
+
+	command_handler := cmd.Cmd{DB: s.DB, Conn: conn}
+	reader := bufio.NewReader(conn)
+
+	for {
+		lengthBuf := make([]byte, 4)
+		_, err := io.ReadFull(reader, lengthBuf)
+
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("Failed to read length , %v", err)
+			}
+			return
+		}
+
+		length := binary.BigEndian.Uint32(lengthBuf)
+		if length == 0 || length > 100*1024*1024 {
+			log.Printf("Invalid length %d", length)
+			return
+		}
+
+		payload := make([]byte, length)
+
+		_, err = io.ReadFull(reader, payload)
+
+		if err != nil {
+			log.Printf("Failed to read the payload %v", err)
+			return
+		}
+
+		var raw_tcp_message structs.TCPMessage
+
+		if err := json.Unmarshal(payload, &raw_tcp_message); err != nil {
+			log.Printf("Invalid JSON: %v", err)
+			return
+		}
+
+		switch raw_tcp_message.Msg {
+		case "add-profiles":
+			var data structs.AddProfilesData
+			if err := json.Unmarshal(raw_tcp_message.Data, &data); err != nil {
+				log.Printf("Invalid body for add-profiles %v", err)
+				return
+			}
+			command_handler.AddProfiles(data)
+
+		case "delete-profiles":
+			var data structs.DeleteProfilesData
+			if err := json.Unmarshal(raw_tcp_message.Data, &data); err != nil {
+				log.Printf("Invalid body for delete-profiles %v", err)
+				return
+			}
+			command_handler.DeleteProfiles(data)
+
+		default:
+			log.Println("Message not supported")
+		}
+	}
+}
--- core/main.go
+++ core/main.go
@@ -1,99 +1,14 @@
 package main
 
 import (
-	"bufio"
-	cmd "bushuray-core/commands"
 	"bushuray-core/db"
-	"bushuray-core/structs"
-	"encoding/binary"
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"net"
-	"os"
+	"bushuray-core/lib/TCPServer"
 )
-
 
 func main() {
 	database := db.DB{}
 	database.Initialize()
-	listen, err := net.Listen("tcp", "127.0.0.1:4897")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(0)
-	}
-
-	fmt.Println("server is listening on port 4897")
-
-	for {
-		conn, err := listen.Accept()
-
-		if err != nil {
-			log.Printf("failed to accept connection: %v", err)
-		}
-
-		go handleConnection(conn, &database)
-	}
-}
-
-func handleConnection(conn net.Conn, database *db.DB) {
-	defer conn.Close()
-	command_handler := cmd.Cmd{DB: database, Conn: conn}
-	reader := bufio.NewReader(conn)
-
-	for {
-		lengthBuf := make([]byte, 4)
-		_, err := io.ReadFull(reader, lengthBuf)
-
-		if err != nil {
-			if err != io.EOF {
-				log.Printf("Failed to read length , %v", err)
-			}
-			return
-		}
-
-		length := binary.BigEndian.Uint32(lengthBuf)
-		if length == 0 || length > 100*1024*1024 {
-			log.Printf("Invalid length %d", length)
-			return
-		}
-
-		payload := make([]byte, length)
-
-		_, err = io.ReadFull(reader, payload)
-
-		if err != nil {
-			log.Printf("Failed to read the payload %v", err)
-			return
-		}
-
-		var raw_tcp_message structs.TCPMessage
-
-		if err := json.Unmarshal(payload, &raw_tcp_message); err != nil {
-			log.Printf("Invalid JSON: %v", err)
-			return
-		}
-
-		switch raw_tcp_message.Msg {
-		case "add-profiles":
-			var data structs.AddProfilesData
-			if err := json.Unmarshal(raw_tcp_message.Data, &data); err != nil {
-				log.Printf("Invalid body for add-profiles %v", err)
-				return
-			}
-			command_handler.AddProfiles(data)
-
-		case "delete-profiles":
-			var data structs.DeleteProfilesData
-			if err := json.Unmarshal(raw_tcp_message.Data, &data); err != nil {
-				log.Printf("Invalid body for delete-profiles %v", err)
-				return
-			}
-			command_handler.DeleteProfiles(data)
-
-		default:
-			log.Println("Message not supported")
-		}
-	}
+	server := TCPServer.NewServer(&database)
+	server.Start()
+	select {}
 }
