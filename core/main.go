1cad7c1c0ab17669ee56d7d386b58c4d97c84a14
Keivan-sf <keyvan0082@gmail.com>
2025-07-30 15:15:48 +0330

    Kill xray core on kill signals

--- core/main.go
+++ core/main.go
@@ -4,6 +4,10 @@ import (
 	"bushuray-core/db"
 	"bushuray-core/lib/TCPServer"
 	"bushuray-core/lib/proxy"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -13,5 +17,13 @@ func main() {
 	proxy_manager.Init()
 	server := TCPServer.NewServer(&database, &proxy_manager)
 	server.Start()
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-sigs
+		log.Printf("Recved signal %v , cleaning up...", sig)
+		proxy_manager.Stop()
+		os.Exit(0)
+	}()
 	select {}
 }
