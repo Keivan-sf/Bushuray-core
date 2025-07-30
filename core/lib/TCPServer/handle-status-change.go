c3578035fb746ba6465aec0b6ab16078de452ccd
Keivan-sf <keyvan0082@gmail.com>
2025-07-30 12:39:35 +0330

    Log connection status

--- core/lib/TCPServer/handle-status-change.go
+++ core/lib/TCPServer/handle-status-change.go
@@ -8,6 +8,7 @@ import (
 func (s *Server) handleStatusChange() {
 	log.Println("listening to connections state change:")
 	for status := range s.proxy_manager.StatusChanged {
+		log.Println("Connection status changed:", status.Connection)
 		s.BroadCast(lib.CreateJsonNotification("status-changed", status))
 	}
 }
