4a2bee721e62bbbedde7d5196885009f38b3edce
Keivan-sf <keyvan0082@gmail.com>
2025-07-31 10:44:28 +0330

    Make http port optional when parsing uri

--- core/lib/parse.go
+++ core/lib/parse.go
@@ -10,7 +10,12 @@ import (
 func ParseUri(uri string, socksport int, httpport int) ([]byte, error) {
 	var parsed_config []byte
 	v2parserbin := path.Join(GetWorkingDir(), "bin", "v2parser")
-	v2parser_parse_cmd := exec.Command(v2parserbin, uri, "--socksport", strconv.Itoa(socksport), "--httpport", strconv.Itoa(httpport))
+	var v2parser_parse_cmd *exec.Cmd
+	if httpport != -1 {
+		v2parser_parse_cmd = exec.Command(v2parserbin, uri, "--socksport", strconv.Itoa(socksport), "--httpport", strconv.Itoa(httpport))
+	} else {
+		v2parser_parse_cmd = exec.Command(v2parserbin, uri, "--socksport", strconv.Itoa(socksport))
+	}
 
 	parsed_config, err := v2parser_parse_cmd.Output()
 	if err != nil {
