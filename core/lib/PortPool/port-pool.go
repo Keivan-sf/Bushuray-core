9152ede9d96c894fae1f6a56a36ddfff87b07a8f
Keivan-sf <keyvan0082@gmail.com>
2025-07-31 10:44:13 +0330

    Resolve race conditions in port pool

--- core/lib/PortPool/port-pool.go
+++ core/lib/PortPool/port-pool.go
@@ -3,20 +3,38 @@ package portpool
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net"
+	"sync"
 )
 
 type PortPool struct {
 	start_port int
 	end_port   int
 	in_use     map[int]bool
+	mu         sync.Mutex
+}
+
+func CreatePortPool(start_port int, end_port int) *PortPool {
+	if start_port < 0 || end_port < 0 || end_port <= start_port {
+		log.Fatal("invalid testing port range")
+	}
+	return &PortPool{
+		start_port: start_port,
+		end_port:   end_port,
+		in_use:     make(map[int]bool),
+	}
 }
 
 func (p *PortPool) ReleasePort(port int) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	p.in_use[port] = false
 }
 
 func (p *PortPool) GetPort() (int, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	for i := p.start_port; i < p.end_port; i++ {
 		if !p.isPortInUse(i) {
 			p.in_use[i] = true
